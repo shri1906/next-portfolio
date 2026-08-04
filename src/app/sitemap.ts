1ac34257ed36a62f3f18dea74e8355d69bcaf543
shri1906 <shivam0626.soi@gmail.com>
2026-08-04 10:18:43 +0530

    sitemap updated updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,17 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: "https://shivammaurya-coder.vercel.app/projects",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: "https://shivammaurya-coder.vercel.app/contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
